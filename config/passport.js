6a9610ed0e1f6cf114d781da8b91a51219f3bfb6
jimin <seojiminca@gmail.com>
2020-03-14 14:40:50 +0900

    add passport-jwt

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+//passport를 이용해서 json web token인증해주는 함수
+
+const {Strategy,ExtractJwt} = require('passport-jwt'); //JWT를 생성하고 푸는 두개의 함수.
+const userModel = require('../model/user');
+
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+
+module.exports = passport => {
+    passport.use(
+        new Strategy(opts, (payload, done) => {
+            userModel
+                .findById(payload.id)
+                .then(user => {
+                    if(user){
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                })
+                .catch(err => {
+                    console.log(err)
+                });
+        })
+    )
+}
