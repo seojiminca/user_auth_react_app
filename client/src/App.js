cca5c3fab3ee613882d02e92be5e4900a2ce5d75
jimin <seojiminca@gmail.com>
2020-05-05 17:22:39 +0900

    add redux store and chrome extension setup

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,25 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import "./App.css";
+import {Provider} from 'react-redux'
+import store from './store';
 
 function App() {
-  return (
-    <Router>
-        <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </div>
-            <Footer/>
-        </div>
-    </Router>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route exact path="/" component={Landing}/>
+                    <div className="container">
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </div>
+                    <Footer/>
+                </div>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
