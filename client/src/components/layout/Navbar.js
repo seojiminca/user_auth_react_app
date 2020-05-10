4a915f155092b89cad440abee1eb31b2d3fc4f45
jimin <seojiminca@gmail.com>
2020-05-10 17:25:01 +0900

    Logout and navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,58 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
+import {logoutUser} from "../../actions/authActions";
 
 
 class Navbar extends Component {
+
+    onLogoutClick(e) {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
+
     render() {
+
+        const {isAuthenticated, user} = this.props.auth;
+
+        const authLinks = (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <a
+                        href=""
+                        onClick={this.onLogoutClick.bind(this)}
+                        className="nav-link"
+                    >
+                        <img
+                            className="rounded-circle"
+                            src={user.avatar}
+                            alt={user.name}
+                            //{{}} 스타일 바로 적용.
+                            style={{width: '25px', marginRight: '5px'}}
+                            title='You must have a Gravatar connected to your email to display an image'
+                        />{' '}
+                        Logout
+                    </a>
+                </li>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className='nav-link' to="/register">
+                        Sign up
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className='nav-link' to="/login">
+                        Log in
+                    </Link>
+                </li>
+            </ul>
+        );
+
         return (
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
                 <div className="container">
@@ -18,7 +67,7 @@ class Navbar extends Component {
                         data-target="#mobile-nav"
                     >
                         {/*리액트에서는 className. html에서는 class*/}
-                        <span className="nav-bar-toggler-icon" />
+                        <span className="nav-bar-toggler-icon"/>
                     </button>
                 </div>
 
@@ -26,27 +75,26 @@ class Navbar extends Component {
                 <div className="collapse navbar-collapse" id="mobile-nav">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <Link className='nav-link' to="/">
+                            <Link className="nav-link" to="/profiles">
+                                {' '}
                                 Developers
                             </Link>
                         </li>
                     </ul>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className='nav-link' to="/register">
-                                Sign up
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className='nav-link' to="/login">
-                                Log in
-                            </Link>
-                        </li>
-                    </ul>
+                    {isAuthenticated ? authLinks : guestLinks}
                 </div>
             </nav>
         );
     }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, {logoutUser})(Navbar);
