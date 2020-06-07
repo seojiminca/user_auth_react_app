35e527ec3c4c9f49484d690f6137b121ab86bbd7
jimin <seojiminca@gmail.com>
2020-06-07 14:14:45 +0900

    heroku modify

--- server.js
+++ server.js
@@ -38,6 +38,21 @@ if(process.env.NODE_ENV || "production" === 'production'){
     })
 }
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS");
+    next();
+});
+
+app.use(async function(err, req, res, next) {
+    console.error(err.message);
+    await res.status(500).json({
+        error: err.message
+    });
+    next();
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`)); // ``로 자바스크립트 불러오기
